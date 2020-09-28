2b12b4be7b05034a4de0a3ef3916d8d1f33a4343
agosjordan <agos.jordan@hotmail.com>
2020-09-28 11:04:29 -0300

    footer

--- proyecto.php
+++ proyecto.php
@@ -95,29 +95,10 @@
                     </div>
                 </div>    
             </div>
-
         </div>
-
-
     </main>
-    <footer>
-        <div class="container pt-5">
-            <div class="row">
-                <div class="col-sm-3 col-12 text-sm-left text-center pb-3 pb-sm-0">
-                    <a href="https://github.com" target="blank"><i class="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/agosjordan/" target="blank"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <div class="col-sm-3 col-12 text-sm-left text-center pb-sm-0 pb-3">
-                    <span id="sponsor">Sponsor: </span> <a href="http://depcsuite.com" target="blank" title="depcsuite">DePc suite</a>
-                </div>
-                <div class="col-sm-3 col-12 text-sm-left text-center">
-                    <a href="mailito:agos.jordan@hotmail.com">agos.jordan@hotmail.com</a>
-                </div>
-                <div class="col-sm-3 col-12 text-right">
-                    <a href="https://api.whatsapp.com/send?phone=541134131093" target="blank" title="whatsapp"><i class="fab fa-whatsapp"></i></a>
-                </div>
-            </div>
-        </div>
+    <footer  class="pt-5">
+        <?php include_once("footer.php"); ?>
     </footer>
 </body>
 </html>
